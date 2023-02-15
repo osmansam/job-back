b7191e0708b8a5045a88793bb9f8cebc50fbee5f
Osman <114867365+osmansam@users.noreply.github.com>
2023-02-15 15:52:35 -0500

    get all jobs function modified

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -4,10 +4,12 @@ const CustomError = require("../errors");
 const mongoose = require("mongoose");
 const moment = require("moment");
 
+//get all jobs function will be reviesed to make the search function work
 const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.userId });
   res.status(StatusCodes.OK).json({ jobs });
 };
+//get job by id
 const getJob = async (req, res) => {
   const job = await Job.findById({
     _id: req.params.id,
@@ -18,11 +20,13 @@ const getJob = async (req, res) => {
   }
   res.status(StatusCodes.OK).json({ job });
 };
+//create job
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
   res.status(StatusCodes.CREATED).json({ job });
 };
+//update job
 const updateJob = async (req, res) => {
   const { company, position } = req.body;
   if (!company || !position) {
@@ -40,6 +44,7 @@ const updateJob = async (req, res) => {
   }
   res.status(StatusCodes.OK).json({ job });
 };
+//delete job
 const deleteJob = async (req, res) => {
   const job = await Job.findById({
     _id: req.params.id,
