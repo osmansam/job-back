5a5ef0c1d051ec9a44d8c499985ddfb93ec5707b
Osman <114867365+osmansam@users.noreply.github.com>
2023-02-20 21:56:23 -0500

    page limit changed for testing

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -37,7 +37,7 @@ const getAllJobs = async (req, res) => {
   }
 
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
+  const limit = Number(req.query.limit) || 4;
   const skip = (page - 1) * limit;
 
   result = result.skip(skip).limit(limit);
