eb257bae1e00f2a16d6f77d9296db46d1adb73ef
Osman <114867365+osmansam@users.noreply.github.com>
2023-02-21 14:56:05 -0500

    employee and employer arrangment are made

--- models/User.js
+++ models/User.js
@@ -25,7 +25,7 @@ const UserSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ["admin", "user"],
+    enum: ["admin", "employer", "employee"],
     default: "user",
   },
   verificationToken: String,
